db4167300e8b24aeac23f8bb3b86b4b8727dca51
hd-chuong <hodacthanhchuong@gmail.com>
2024-04-22 16:27:59 +1000

    simplify logic

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ class Model:
 
     def __init__(self):
         self.__dump_temp = 'dump.png'
+        
 
     def infer(self, link):
         image = cv2.imread(link)
@@ -43,13 +44,12 @@ class Model:
         ann = prompt_process.point_prompt(points=[[int(width/2),int(height/2)]], pointlabel=[1])
 
         binary_mask = np.where(ann > 0.5, 1, 0)
+
+
         print("LOG - generate new image:")
+        print(binary_mask[0])
         new_image = image * binary_mask[0][..., np.newaxis]
-    
-        alpha = np.sum(new_image, axis=-1) > 0
-        # Convert True/False to 0/255 and change type to "uint8" to match "na"
-        alpha = np.uint8(alpha * 255)
-        image_with_transparent_background = np.dstack((new_image,alpha ))
+        image_with_transparent_background = np.dstack((new_image, binary_mask[0]*255))
         # TODO: might need to return the base64 data back
         print("LOG - save segmentation data")
         return cv2.imwrite("../output/output.png", image_with_transparent_background)
